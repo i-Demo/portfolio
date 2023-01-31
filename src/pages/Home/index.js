a372a4969ebe17913d0769d4273afce282dc9821
Hoai Vi <hoaivi.it@gmail.com>
2023-01-31 16:43:55 +0700

    Build Portfolio Page #1

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -27,12 +27,12 @@ function Home() {
                             <span>Web Developer !</span>
                         </h1>
                         <p>
-                            Hello mọi người! Mình là Vĩ, đang là sinh viên năm 2 trường Đại học Giao thông vận tải Tp.
-                            HCM. Cơ duyên mình đến với IT là nhờ các sự tích "IT là vua của các nghề", hay câu chuyện
-                            "thằng em học BK cơ khí sinh năm 96". Tháng 5/2020, tình cờ biết đến cộng đồng F8 Fullstack
-                            của anh Sơn, mình đã hoàn thành các khóa học HTML, CSS, ReactJS và có được một số dự án cá
-                            nhân. Trải qua nhiều cay đắng, thật sự cảm ơn anh Sơn cũng như cộng đồng anh em đóng góp đã
-                            truyền động lực cho mình suốt thời gian qua giúp mình không bỏ cuộc.
+                            Hello mọi người! Mình là Vĩ, đang là sinh viên năm cuối trường Đại học Giao thông vận tải
+                            Tp. HCM. Cơ duyên mình đến với IT là nhờ các sự tích "IT là vua của các nghề", hay câu
+                            chuyện "thằng em học BK cơ khí sinh năm 96". Tháng 5/2020, tình cờ biết đến cộng đồng F8
+                            Fullstack của anh Sơn, mình đã hoàn thành các khóa học HTML, CSS, ReactJS và có được một số
+                            dự án cá nhân. Trải qua nhiều cay đắng, thật sự cảm ơn anh Sơn cũng như cộng đồng anh em
+                            đóng góp đã truyền động lực cho mình suốt thời gian qua giúp mình không bỏ cuộc.
                         </p>
 
                         <Button to="/portfolio" className={cx('home-btn')}>
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,5 +1,24 @@
+import classNames from 'classnames/bind';
+import styles from './Portfolio.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Portfolio() {
-    return <h2>Portfolio Page</h2>;
+    return (
+        <div className={cx('main')}>
+            <div className={cx('page-header')}>
+                <h1>
+                    <span>My </span>
+                    portfolio
+                </h1>
+                <span>Works</span>
+            </div>
+
+            <div classNames={cx('container')}>
+                <div className={cx('tabs')}></div>
+            </div>
+        </div>
+    );
 }
 
 export default Portfolio;
