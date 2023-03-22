233b45c10f2c67d14e38c47034d6685cee5296ba
Hoai Vi <hoaivi.it@gmail.com>
2023-03-22 17:36:49 +0700

    Update Animation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import Button from '~/components/Button';
 import Image from '~/components/Image';
@@ -10,6 +11,25 @@ import styles from './Home.module.scss';
 const cx = classNames.bind(styles);
 
 function Home() {
+    const [personalInfo] = useTypewriter({
+        words: ['Hoài Vĩ.', 'a Fullstack Developer !'],
+        loop: 0,
+        typeSpeed: 120,
+        deleteSpeed: 80,
+    });
+    const [infoPerson] = useTypewriter({
+        words: [
+            `Hello! I’m Vĩ. My full name is Hồ Lê Hoài Vĩ. I’m 23. I graduated from Ho Chi Minh City
+        University of Transport in 2023 with a bachelor’s degree in Infomation Technology major.
+        With over 2 years of studying and practising as a Frontend Developer, I’m confident
+        about my professional skills and my soft skills. I believe that if we want to work
+        effectively and develop ourselves, carefulness and creativity is very important. That’s
+        what I would like to share about myself.`,
+        ],
+        loop: 1,
+        typeSpeed: 1,
+    });
+
     return (
         <div>
             <div className="slide"></div>
@@ -26,17 +46,14 @@ function Home() {
 
                     <div className={cx('right-home')}>
                         <div className={cx('right-wrapper')}>
+                            <h2>Hello, </h2>
                             <h1>
-                                I'm Hoai Vi
-                                <span>Web Developer !</span>
+                                I'm <span>{personalInfo}</span>
+                                <Cursor cursorStyle="<" />
                             </h1>
                             <p>
-                                Hello! I’m Vĩ. My full name is Hồ Lê Hoài Vĩ. I’m 23. I graduated from Ho Chi Minh City
-                                University of Transport in 2023 with a bachelor’s degree in Infomation Technology major.
-                                With over 2 years of studying and practising as a Frontend Developer, I’m confident
-                                about my professional skills and my soft skills. I believe that if we want to work
-                                effectively and develop ourselves, carefulness and creativity is very important. That’s
-                                what I would like to share about myself.
+                                {infoPerson}
+                                <Cursor cursorStyle="_" />
                             </p>
 
                             <div className={cx('btn-block')}>
